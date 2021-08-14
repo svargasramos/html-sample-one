776833bb11cfb1be7385b4060fd9c8199ee7afe0
evargas <evargas@sparkdigital.com>
2021-08-13 22:15:33 -0300

    change services

--- components/header.php
+++ components/header.php
@@ -17,7 +17,7 @@
     <ul>
 
         <a href="index.php"><li class="botones">Boton 1</li></a>
-        <a href="./pages/services.php"><li class="botones">Boton 2</li></a>
+        <a href="../services.php"><li class="botones">Boton 2</li></a>
         <a href="./html03.php"><li class="botones">Boton 3</li></a>
         <a href="./Diseño-web-responsivo.php"><li class="botones">Boton 4</li></a>
         <a href="./propied-de-ubicacion.php"><li class="botones">Boton 5</li></a>
