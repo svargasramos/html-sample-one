79e3f859f11dfc270cfb0cbe13aa8c069654aa38
evargas <evargas@sparkdigital.com>
2021-08-25 18:59:36 -0300

    flex

---
+++ html04.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica Flex</title>
+    <link rel="stylesheet" href="./css/html03.css">
+</head>
+<body>
+
+        <header>Cabezote</header>
+
+        <section>
+
+            <aside id="izq">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit officia nemo eveniet sit ad! Fuga nam est ipsam voluptatum alias quis dolorem dignissimos expedita dolor deserunt ea, consequuntur nihil possimus.</aside>
+
+            <article>
+
+                <fieldset class="fieldset-section">
+                    <legend><h2>flex-direction</h2></legend>
+
+                    <div class="flex-container ejemplo0">
+                        <div class="flex-item item1">1</div>
+                        <div class="flex-item item2">2</div>
+                        <div class="flex-item item3">3</div>
+                        <div class="flex-item item4">4</div>
+                    </div>
+
+                </fieldset>
+
+                <fieldset class="fieldset-section">
+                    <legend><h2>flex-wrap</h2></legend>
+
+                    <div class="flex-container ejemplo1">
+                        <div class="flex-item item1">1</div>
+                        <div class="flex-item item2">2</div>
+                        <div class="flex-item item3">3</div>
+                        <div class="flex-item item4">4</div>
+                    </div>
+
+                </fieldset>
+                <fieldset class="fieldset-section">
+                    <legend><h2>justify-content</h2></legend>
+                    <div class="flex-container ejemplo2">
+                        <div class="flex-item item1">1</div>
+                        <div class="flex-item item2">2</div>
+                        <div class="flex-item item3">3</div>
+                        <div class="flex-item item4">4</div>
+                        <div class="flex-item item5">5</div>
+                    </div>
+                </fieldset>
+                <fieldset class="fieldset-section">
+                    <legend><h2>align-items</h2></legend>
+                </fieldset>
+                
+            </article>
+
+        </section>
+        
+  
+        <footer>Pie de Pagina</footer>
+
+</body>
+</html>
